4b811f345947056cb0952ae18e3e746a65c36a66
Arpit <arpitdubey907@gmail.com>
2022-03-28 16:58:30 +0530

    implemented search featue and API integration

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/UI/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/UI/fragments/SearchNewsFragment.kt
@@ -1,14 +1,78 @@
 package com.androiddevs.mvvmnewsapp.ui.UI.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.ui.adapters.NewsAdapter
+import com.androiddevs.mvvmnewsapp.ui.util.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.android.synthetic.main.fragment_search_news.paginationProgressBar
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment:Fragment(R.layout.fragment_search_news) {
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter:NewsAdapter
+    val TAG ="SearchNewsFragment"
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel=(activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        var job: Job?=null
+        etSearch.addTextChangedListener {editabe->
+            job?.cancel()
+            job= MainScope().launch {
+                delay(500L)
+                editabe?.let{
+                    if(editabe.toString().isNotEmpty()){
+                        viewModel.searchNews(editabe.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer{ response ->
+            when(response){
+                is Resource.Success<*> ->{
+                    hideProgressBar()
+                    response.data?.let{
+                            newsResponse->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error ->{
+                    hideProgressBar()
+                    response.message?.let{message ->
+                        Log.e(TAG,"An error occured: $message")
+                    }
+                }
+                is Resource.Loading ->{
+                    showProgressBar()
+                }
+            }
+
+        })
+    }
+    private fun hideProgressBar(){
+        paginationProgressBar.visibility =View.INVISIBLE
+    }
+    private fun showProgressBar(){
+        paginationProgressBar.visibility=View.VISIBLE
+    }
+    private fun setupRecyclerView(){
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter=newsAdapter
+            layoutManager= LinearLayoutManager(activity)
+        }
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/repository/NewsRepository.kt
@@ -1,6 +1,14 @@
 package com.androiddevs.mvvmnewsapp.ui.repository
 
+import com.androiddevs.mvvmnewsapp.ui.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.ui.db.ArticleDatabase
+import retrofit2.Retrofit
 
 class NewsRepository(val db: ArticleDatabase) {
+
+    suspend fun getBrakingNews(countryCode:String,pageNumber:Int) =
+        RetrofitInstance.api.getBreakingNews(countryCode,pageNumber)
+
+    suspend fun searchNews(SearchQuery:String,pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(SearchQuery,pageNumber)
 }
